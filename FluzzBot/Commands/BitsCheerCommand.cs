799f16b8c7a6d7ac5f6608e68b33f7842efc8ac2
Unknown <fluzzarn@gmail.com>
2017-11-26 00:33:05 -0500

    Added geoff's bitcommand

--- FluzzBot/Commands/BitsCheerCommand.cs
+++ FluzzBot/Commands/BitsCheerCommand.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FluzzBot
@@ -19,6 +20,14 @@ namespace FluzzBot
         {
             if(username == "misskaddykins")
             bot.ConstructAndEnqueueMessage("kadCheer kadCheer THANKS FOR THE BITTIES kadCheer kadCheer",username);
+            else if(username == "geoff")
+            {
+                Regex re = new Regex(@"vohiyo\d");
+                if(re.Matches(message.ToLower()).Count > 0)
+                {
+                    bot.ConstructAndEnqueueMessage("gibeCheer gibeCheer WutFace GOD DAMN WEEB BITS WutFace gibeCheer gibeCheer", username);
+                }
+            }
             return true;
         }
     }
